bf8b1a3c2275fcdce8191d2198901c3d49e3aa47
Erik Petrov <koteman123@hotmail.com>
2021-06-11 00:38:26 +0300

    teemo crash gay 228

---
+++ Content/LeagueSandbox-Scripts/Buffs/Teemo/MoveQuick.cs
@@ -0,0 +1,39 @@
+﻿using GameServerCore.Enums;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using GameServerCore.Domain.GameObjects;
+using GameServerCore.Domain.GameObjects.Spell;
+using LeagueSandbox.GameServer.GameObjects.Stats;
+using GameServerCore.Scripting.CSharp;
+
+namespace MoveQuick
+{
+    internal class MoveQuick : IBuffGameScript
+{
+        public BuffType BuffType => BuffType.HASTE;
+
+        public BuffAddType BuffAddType => BuffAddType.STACKS_AND_OVERLAPS;
+
+        public int MaxStacks => 1;
+
+        public bool IsHidden => false;
+
+        public IStatsModifier StatsModifier { get; private set; } = new StatsModifier();
+
+        //TODO: particles..?
+        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+            StatsModifier.MoveSpeed.PercentBonus += (12f + ownerSpell.CastInfo.SpellLevel * 8) / 100f;
+            unit.AddStatModifier(StatsModifier);
+        }
+
+        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+
+        }
+
+        public void OnUpdate(float diff)
+        {
+
+        }
+    }
+}
---
+++ Content/LeagueSandbox-Scripts/Champions/Teemo/W.cs
@@ -0,0 +1,62 @@
+﻿using System.Numerics;
+using GameServerCore.Enums;
+using GameServerCore.Domain.GameObjects;
+using GameServerCore.Domain.GameObjects.Spell;
+using GameServerCore.Domain.GameObjects.Spell.Missile;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using GameServerCore.Scripting.CSharp;
+
+namespace Spells
+{
+    public class MoveQuick : ISpellScript
+    {
+        public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        {
+            TriggersSpellCasts = true
+            // TODO
+        };
+
+        public void OnActivate(IObjAiBase owner, ISpell spell)
+        {
+        }
+
+        public void OnDeactivate(IObjAiBase owner, ISpell spell)
+        {
+        }
+
+        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
+        {
+            AddBuff("MoveQuick", 3.0f, 1, spell, target, owner);
+        }
+
+        public void OnSpellCast(ISpell spell)
+        {
+        }
+
+        public void OnSpellPostCast(ISpell spell)
+        {
+        }
+
+        public void ApplyEffects(IObjAiBase owner, IAttackableUnit target, ISpell spell, ISpellMissile missile)
+        {
+         
+        }
+
+        public void OnSpellChannel(ISpell spell)
+        {
+        }
+
+        public void OnSpellChannelCancel(ISpell spell)
+        {
+        }
+
+        public void OnSpellPostChannel(ISpell spell)
+        {
+        }
+
+        public void OnUpdate(float diff)
+        {
+        }
+    }
+}
